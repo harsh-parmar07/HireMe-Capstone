e6e219945c0efda41d6552ec321552dcab1bc576
Harsh Parmar <n01691527@humber.ca>
2025-06-08 19:10:10 -0400

    folders and some files added

---
+++ includes/db.php
@@ -0,0 +1,4 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "hireme");
+if ($conn->connect_error) die("Database connection failed");
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include("includes/header.php"); ?>
+<h1>HireMe: Online Freelance Marketplace</h1>
+<p>This platform will connect freelancers and clients.</p>
+<a href="auth/register.php">Register</a> |
+<a href="auth/login.php">Login</a>
+<?php include("includes/footer.php"); ?>
